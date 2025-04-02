43f12c395f9f42faa28b8ee1a710b4e9a528b856
pj <pj.vandenbussche@gmail.com>
2025-04-02 22:02:31 +0200

    run two containers

--- test/index.test.mjs
+++ test/index.test.mjs
@@ -2,20 +2,24 @@ import { describe, beforeAll, beforeEach, afterAll, afterEach, it, expect } from
 import { PostgreSqlContainer } from "@testcontainers/postgresql";
 
 describe('repro', () => {
-  let postgresContainer;
+  let one;
+  let two;
 
   beforeAll(async () => {
-    postgresContainer = await new PostgreSqlContainer("postgres:16-alpine").start();
-
-    await postgresContainer.snapshot();
+    one = await new PostgreSqlContainer("postgres:16-alpine").start();
+    await one.snapshot();
+    two = await new PostgreSqlContainer("postgres:16-alpine").start();
+    await two.snapshot();
   }, 60000);
 
   afterEach(async () => {
-    await postgresContainer.restoreSnapshot();
+    await one.restoreSnapshot();
+    await two.restoreSnapshot();
   }, 60000);
 
   afterAll(async () => {
-    await postgresContainer.stop();
+    await one.stop();
+    await two.stop();
   }, 60000);
 
   it('should run', () => {
