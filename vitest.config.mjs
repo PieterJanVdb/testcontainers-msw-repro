bf05a79b12b013ca0a089b9883194a1fca15dbb1
pj <pj.vandenbussche@gmail.com>
2025-04-02 22:50:25 +0200

    set env DEBUG

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
+    env: {
+      DEBUG: "testcontainers:*",
+    },
     setupFiles: ['./test/setup.ts'],
     poolOptions: {
       forks: {
