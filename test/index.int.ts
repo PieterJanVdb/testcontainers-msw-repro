42aca75666ca5c18ca687d7c445f1e1970050f7c
pj <pj.vandenbussche@gmail.com>
2025-04-02 23:52:41 +0200

    run just one

--- test/index.int.ts
+++ test/index.int.ts
@@ -6,24 +6,18 @@ import {
 
 describe("repro", () => {
   let containerOne: StartedPostgreSqlContainer;
-  let containerTwo: StartedPostgreSqlContainer;
 
   beforeAll(async () => {
     containerOne = await new PostgreSqlContainer("postgres:16-alpine").start();
     await containerOne.snapshot();
-
-    containerTwo = await new PostgreSqlContainer("postgres:16-alpine").start();
-    await containerTwo.snapshot();
   }, 60000);
 
   afterEach(async () => {
     await containerOne?.restoreSnapshot();
-    await containerTwo?.restoreSnapshot();
   }, 60000);
 
   afterAll(async () => {
     await containerOne?.stop();
-    await containerTwo?.stop();
   }, 60000);
 
   it("should run", () => {
